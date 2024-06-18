c549d78ea90fe0843215357a7bbe036163679af9
Arturo Buzarra <arturo.buzarra@digi.com>
2024-06-18 13:30:29 +0200

    ccmp25-dvk: include Digi custom functionality to ConnectCore MP2 platform
    https://onedigi.atlassian.net/browse/DEL-8790
    
    Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

--- include/configs/ccmp25-dvk.h
+++ include/configs/ccmp25-dvk.h
@@ -6,80 +6,14 @@
  * Configuration settings for the STM32MP25x CPU
  */
 
-#ifndef __CONFIG_STM32MP25_COMMMON_H
-#define __CONFIG_STM32MP25_COMMMON_H
-#include <linux/sizes.h>
-#include <asm/arch/stm32.h>
+#ifndef __CONFIG_CCMP25_DVK_COMMMON_H
+#define __CONFIG_CCMP25_DVK_COMMMON_H
 
-/*
- * Configuration of the external SRAM memory used by U-Boot
- */
-#define CONFIG_SYS_SDRAM_BASE		STM32_DDR_BASE
+#include <configs/ccmp2_common.h>
 
-/*
- * For booting Linux, use the first 256 MB of memory, since this is
- * the maximum mapped by the Linux kernel during initialization.
- */
-#define CONFIG_SYS_BOOTMAPSZ		SZ_256M
-
-/* MMC */
-#define CONFIG_SYS_MMC_MAX_DEVICE	3
-
-/* NAND support */
-#define CONFIG_SYS_MAX_NAND_DEVICE	1
-
-/* CFI support */
-#define CONFIG_SYS_MAX_FLASH_BANKS	1
-#define CONFIG_CFI_FLASH_USE_WEAK_ACCESSORS
-
-/* Ethernet need */
-#ifdef CONFIG_DWC_ETH_QOS
-#define CONFIG_SERVERIP                 192.168.1.1
-#endif
-
-#define STM32MP_FIP_IMAGE_GUID \
-	EFI_GUID(0x19d5df83, 0x11b0, 0x457b, 0xbe, 0x2c, \
-		 0x75, 0x59, 0xc1, 0x31, 0x42, 0xa5)
-
-/*****************************************************************************/
-#ifdef CONFIG_DISTRO_DEFAULTS
-/*****************************************************************************/
-
-#ifdef CONFIG_NET
-#define BOOT_TARGET_PXE(func)	func(PXE, pxe, na)
-#else
-#define BOOT_TARGET_PXE(func)
-#endif
-
-#ifdef CONFIG_CMD_MMC
-#define BOOT_TARGET_MMC0(func)	func(MMC, mmc, 0)
-#define BOOT_TARGET_MMC1(func)	func(MMC, mmc, 1)
-#define BOOT_TARGET_MMC2(func)	func(MMC, mmc, 2)
-#else
-#define BOOT_TARGET_MMC0(func)
-#define BOOT_TARGET_MMC1(func)
-#define BOOT_TARGET_MMC2(func)
-#endif
-
-#ifdef CONFIG_CMD_UBIFS
-#define BOOT_TARGET_UBIFS(func)	func(UBIFS, ubifs, 0, UBI, boot)
-#else
-#define BOOT_TARGET_UBIFS(func)
-#endif
-
-#ifdef CONFIG_CMD_USB
-#define BOOT_TARGET_USB(func)	func(USB, usb, 0)
-#else
-#define BOOT_TARGET_USB(func)
-#endif
-
-#define BOOT_TARGET_DEVICES(func)	\
-	BOOT_TARGET_MMC1(func)		\
-	BOOT_TARGET_UBIFS(func)		\
-	BOOT_TARGET_MMC0(func)		\
-	BOOT_TARGET_MMC2(func)		\
-	BOOT_TARGET_USB(func)		\
-	BOOT_TARGET_PXE(func)
+#define CONFIG_SOM_DESCRIPTION		"ConnectCore MP25"
+#define CONFIG_BOARD_DESCRIPTION	"Development Kit"
+#define BOARD_DEY_NAME			"ccmp25-dvk"
 
 /*
  * default bootcmd for stm32mp25:
@@ -109,29 +43,6 @@
 #define STM32MP_EXTRA \
 	"env_check=if env info -p -d -q; then env save; fi\0" \
 	"boot_net_usb_start=true\0"
-/*
- * memory layout for 64M uncompressed/compressed kernel,
- * 1M fdt, 1M script, 1M pxe and 1M for overlay
- * and the ramdisk at the end.
- */
-#define __KERNEL_COMP_ADDR_R	__stringify(0x84000000)
-#define __KERNEL_COMP_SIZE_R	__stringify(0x04000000)
-#define __KERNEL_ADDR_R		__stringify(0x88000000)
-#define __FDT_ADDR_R		__stringify(0x8c000000)
-#define __SCRIPT_ADDR_R		__stringify(0x8c100000)
-#define __PXEFILE_ADDR_R	__stringify(0x8c200000)
-#define __FDTOVERLAY_ADDR_R	__stringify(0x8c300000)
-#define __RAMDISK_ADDR_R	__stringify(0x8c400000)
-
-#define STM32MP_MEM_LAYOUT \
-	"kernel_addr_r=" __KERNEL_ADDR_R "\0" \
-	"fdt_addr_r=" __FDT_ADDR_R "\0" \
-	"scriptaddr=" __SCRIPT_ADDR_R "\0" \
-	"pxefile_addr_r=" __PXEFILE_ADDR_R "\0" \
-	"fdtoverlay_addr_r=" __FDTOVERLAY_ADDR_R "\0" \
-	"ramdisk_addr_r=" __RAMDISK_ADDR_R "\0" \
-	"kernel_comp_addr_r=" __KERNEL_COMP_ADDR_R "\0"	\
-	"kernel_comp_size=" __KERNEL_COMP_SIZE_R "\0"
 
 #include <config_distro_bootcmd.h>
 #define CONFIG_EXTRA_ENV_SETTINGS \
@@ -141,6 +52,14 @@
 	STM32MP_EXTRA \
 	STM32MP_BOARD_EXTRA_ENV
 
-#endif
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND \
+	"if run loadscript; then " \
+		"if test \"${dboot_kernel_var}\" = fitimage; then " \
+			"source ${loadaddr}:${fit-script};" \
+		"else " \
+			"source ${loadaddr};" \
+		"fi;" \
+	"fi;"
 
-#endif /* __CONFIG_STM32MP25_COMMMON_H */
+#endif /* __CONFIG_CCMP25_DVK_COMMMON_H */
